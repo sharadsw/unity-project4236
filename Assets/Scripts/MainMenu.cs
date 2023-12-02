78a357feef6f22a42c84314c1ecfa5aff64ed6d7
Sharad <sharad97@gmail.com>
2023-12-02 01:23:03 -0500

    connect main menu with game scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
         Debug.Log("Starting game");
+        SceneManager.LoadScene("TestScene");
     }
     public void QuitGame() 
     {
